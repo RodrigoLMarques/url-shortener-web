5082f1146b83ffc3b0dbef93d71dd67fdd2d656e
RodrigoLMarques <rodrigolopes.m2005@gmail.com>
2025-10-08 16:54:12 -0300

    fix: allowed hosts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  preview: {
+    host: true,
+    port: 4173,
+    allowedHosts: process.env.VITE_ALLOWED_HOSTS
+      ? process.env.VITE_ALLOWED_HOSTS.split(",")
+      : ["localhost"],
+  },
+});
