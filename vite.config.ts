0cb70577600ee288907d57c37d674461b4ee5a89
RodrigoLMarques <rodrigolopes.m2005@gmail.com>
2025-10-08 17:00:13 -0300

    fix: vite config base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/web/",
   preview: {
     host: true,
     port: 4173,
